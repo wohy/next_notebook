e26597e18eb97abc4c79a1ccb479895513081d2d
wohy <1829757133@qq.com>
2024-11-30 18:30:26 +0800

    编辑认证

--- i18n.ts
+++ i18n.ts
@@ -1,10 +1,12 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { getRequestConfig } from 'next-intl/server';
 import { locales } from '@/config'
 
 export default getRequestConfig(async ({ requestLocale }) => {
-    const locale = (await requestLocale) || ''
-    if (!locales.includes(locale)) notFound();
+    let locale = (await requestLocale) || ''
+    if (!locales.includes(locale)) {
+        locale = 'zh'
+    }
 
     return {
         messages: (await import(`./messages/${locale}.json`)).default
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,33 @@
 import createMiddleware from 'next-intl/middleware';
 import { locales, defaultLocale } from '@/config';
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from 'auth';
 
-export default createMiddleware({
+const localeMiddleware = createMiddleware({
     locales,
     defaultLocale,
     // 默认语言不重定向
     localePrefix: 'as-needed'
 });
+
+export async function middleware(req: NextRequest): Promise<NextResponse> {
+
+    // 执行认证中间件
+    const session = await auth();
+    // 获取请求的路径
+    const { pathname } = req.nextUrl;
+
+    // 如果是需要认证的路径（如 /note/edit），且没有会话，返回 401
+    if (pathname.startsWith('/note/eidt') && session === null) {
+        return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    // 执行语言中间件
+    const localeResponse = localeMiddleware(req);
+    if (localeResponse) return localeResponse;  // 如果语言中间件返回响应（如重定向）
+    // 如果都没返回响应，继续请求
+    return NextResponse.next();
+}
 
 export const config = {
     matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
